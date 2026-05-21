52583700b97446b8d10c7ba6c47a1d303d40ed1b
Bart Schelstraete <bart@schelstraete.org>
2026-05-21 03:08:03 +0200

    Bump version to 1.3.2

--- src/constants.ts
+++ src/constants.ts
@@ -7,8 +7,9 @@ export const appTypes = {
   65536: 'playtest'
 }
 
-export const PLUGIN_VERSION = '1.3.0'
+export const PLUGIN_VERSION = '1.3.2'
 
 // ProtonDB Gateway configuration
 export const GATEWAY_BASE_URL = 'https://protondb.schelstraete.org'
-export const GATEWAY_API_KEY = '7fcedef70025704dffa916f4494ac9c9c63a771d6ce528b60afdbb78af296bcf'
+export const GATEWAY_API_KEY =
+  '7fcedef70025704dffa916f4494ac9c9c63a771d6ce528b60afdbb78af296bcf'
